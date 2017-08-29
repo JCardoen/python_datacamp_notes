87af06c2ddd3e34d199e8a2f4cf2ec35a731235e
jcardoen <cardoen.joachim@gmail.com>
2017-08-29 16:23:28 +0200

    WOrking on case

--- python.py
+++ python.py
@@ -2400,9 +2400,51 @@ print(tips.head())
 ########################################################################################################
 
 
+# Write the lambda function using replace
+tips['total_dollar_replace'] = tips.total_dollar.apply(lambda x: x.replace('$', ''))
+
+# Write the lambda function using regular expressions
+tips['total_dollar_re'] = tips.total_dollar.apply(lambda x: re.findall('\d+\.\d+', x)[0])
+
+# Print the head of tips
+print(tips.head())
 
 
+########################################################################################################
+
+# Create the new DataFrame: tracks
+tracks = billboard[['year', 'artist', 'track', 'time']]
+
+# Print info of tracks
+print(tracks.info())
+
+# Drop the duplicates: tracks_no_duplicates
+tracks_no_duplicates = tracks.drop_duplicates()
+
+# Print info of tracks
+print(tracks_no_duplicates.info())
 
 
+########################################################################################################
+
+
+# Calculate the mean of the Ozone column: oz_mean
+oz_mean = airquality.Ozone.mean();
+
+# Replace all the missing values in the Ozone column with the mean
+airquality['Ozone'] = airquality.Ozone.fillna(oz_mean)
+
+# Print the info of airquality
+print(airquality.info())
+
+########################################################################################################
+
+# Assert that there are no missing values
+assert pd.notnull(ebola).all().all()
+
+# Assert that all values are >= 0
+assert (ebola >= 0).all().all()
+
+########################################################################################################
 
 
